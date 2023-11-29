d16d76676e485cf618aea95ab16dc4a04f069410
Besmira75 <besmira.berisha129@gmail.com>
2023-11-29 23:00:30 +0100

    server

--- server.php
+++ server.php
@@ -10,6 +10,20 @@ $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 socket_bind($socket, $server_ip, $server_port);
 
 echo "Server listening on $server_ip:$server_port\n";
+while (true) {
+    socket_recvfrom($socket, $data, 1024, 0, $client_ip, $client_port);
+
+    $pid = pcntl_fork();
+    echo "Received data from $client_ip:$client_port: $data\n";
+
+    if(!checkClient($client_ip)){
+        addClient(new Client($client_ip, $client_port));
+    }
+
+    $response = processRequest($data, $client_ip);
+
+    socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
+}
 
 socket_close($socket);
 ?>
