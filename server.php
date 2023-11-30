e0b6c8ca28367c4c15c5dc513fc647430ef0a5bc
Blerona Jashanica <122100952+bleronaj@users.noreply.github.com>
2023-11-30 18:52:21 +0100

    Fixed Server Responses

--- server.php
+++ server.php
@@ -16,13 +16,55 @@ while (true) {
     $pid = pcntl_fork();
     echo "Received data from $client_ip:$client_port: $data\n";
 
-    if(!checkClient($client_ip)){
-        addClient(new Client($client_ip, $client_port));
-    }
 
     $response = processRequest($data, $client_ip);
 
     socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
+}
+
+function processRequest($request, $client_ip)
+{
+    global $admin_ip;
+    if (substr($request, 0, 1) == "/") {
+        if ($request == "/") {
+            $response = "Command format is: /{Command}. Type /help for the list of commands!";
+        } else if ($request == "/files") {
+            if ($client_ip == $admin_ip) {
+                $files = scandir(__DIR__);
+                $response = "Files on server: " . implode(", ", $files);
+            } else {
+                $response = "You don't have permission for this command!";
+            }
+        } else if ($request == "/help") {
+            $response = "Available commands are:\n/files\n/disconnect";
+        }else if($request=="/disconnect") {
+            $response = "Good bye!";
+        }else if($request == "/clients"){
+            $response = printClients();
+        }else if(substr($request, 0, 5) == "/exec"){
+            $file = substr($request,6);
+            $fileDirectory = __DIR__."/".$file;
+            if (file_exists($fileDirectory)) {
+                exec ($file, $output,$returnCode);
+                if($returnCode === 0){
+                    $response = "The file has been successfully executed!";
+                }else {
+                    $reponse = "There was an error while executing the file! Please try again.";
+                }
+            }else{
+                 $response = "The specified file does not exist!";
+            }
+            
+            
+        }
+         else {
+            $response = "Invalid command!";
+        }
+    } else {
+        $response = "Message received from the server!";
+    }
+
+    return $response;
 }
 
 socket_close($socket);
