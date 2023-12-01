205260a8fa5429676059a96f2ab7d5ab2d68a16c
999BZ <blendizeqiri@hotmail.com>
2023-12-01 22:04:12 +0100

    Added GUI for Client, and fixed bunch of things

--- client.php
+++ client.php
@@ -1,10 +1,146 @@
 <?php
+        $server_ip = "127.0.0.1";
+        $server_port = 12345;
 
-$server_ip = "127.0.0.1";
-$server_port = 12345;
+        $creatingFile = "";
 
-$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+            $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+            $message = explode("|",$_POST['mesazhi'])[0];
+            $creatingFile = explode("|",$_POST['mesazhi'])[1];
+
+            if ($creatingFile != "") {
+                $message = $creatingFile . "," . $message;
+                $creatingFile = "";
+            }
+
+            socket_sendto($socket, $message, strlen($message), 0, $server_ip, $server_port);
+
+            socket_recvfrom($socket, $response, 1024, 0, $server_ip, $server_port);
+
+            switch ($response) {
+                case substr($response, 0, 4) == "/ttf":
+                    $creatingFile = $response;
+                    $response = "Enter text for the file: ";
+                    break;
+            }
+
+            echo $response."|". $creatingFile;
+            socket_close($socket);
+            exit;
+        }
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client</title>
+    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Avatar_icon_green.svg/240px-Avatar_icon_green.svg.png">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            background-color: #f4f4f4;
+        }
+
+        #chatContainer {
+            max-width: 600px;
+            margin: auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        #messages {
+            height: 200px;
+            overflow-y: auto;
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+
+        #messageInput {
+            width: 70%;
+            padding: 8px;
+        }
+
+        #sendButton {
+            width: 20%;
+            padding: 8px;
+            background-color: #4caf50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        #sendButton:hover {
+            background-color: #45a049;
+        }
+        #messages::-webkit-scrollbar {
+            width: 5px;
+        }
+
+        #messages::-webkit-scrollbar-thumb {
+            background-color: #4caf50;
+            border-radius: 2px;
+        }
+
+        #messages::-webkit-scrollbar-thumb:hover {
+            background-color: #45a049;
+        }
+
+        #messages::-webkit-scrollbar-track {
+            background-color: white;
+        }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</head>
+<body>
+    <div id="chatContainer">
+        <div id="messages"></div>
+        <form id="messageForm">
+            <label for="messageInput">Type your message:</label>
+            <input type="text" name="mesazhi" id="messageInput" autocomplete="off">
+            <button type="submit" name="dergoni" id="sendButton">Send</button>
+        </form>
+    </div>
 
 
-socket_close($socket);
-?>
+    <script>
+        $(document).ready(function () {
+            var creatingFiles = "";
+            function sendMessage(message) {
+                $.ajax({
+                    type: "POST",
+                    url: "client.php",
+                    data: { mesazhi: message +"|"+ creatingFiles},
+                    success: function (response) {
+                        var responses = response.split('|');
+                        var messagesContainer = $("#messages");
+                        messagesContainer.append("Me: " +message + "<br>Server: " + responses[0]+ "</br>");
+                        messagesContainer.scrollTop(messagesContainer[0].scrollHeight);
+                        $("#messageInput").val("");
+                        creatingFiles =responses[1];
+                    },
+                    error: function (error) {
+                        console.error("Error:", error);
+                    }
+                });
+            }
+
+            $("#messageForm").submit(function (event) {
+                event.preventDefault();
+                var message = $("#messageInput").val();
+                if(message != ""){
+                    sendMessage(message);
+                }
+                
+            });
+        });
+    </script>
+</body>
+</html>
--- server.php
+++ server.php
@@ -3,30 +3,151 @@
 $server_ip = "0.0.0.0";
 $server_port = 12345;
 
-$admin_ip = "127.0.0.1";
-
-$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-
-class Client
-{
+class Client{
     public $alias;
     public $ip;
-    public $port;
-    public function __construct($ip, $port)
-    {
+    public function __construct($ip){
         $this->ip = $ip;
-        $this->port = $port;
     }
 }
 
 $clients = array();
 
-function checkClient($c_ip)
+$admin_ip = "127.0.0.1";
+
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+socket_bind($socket, $server_ip, $server_port);
+
+echo "Server listening on $server_ip:$server_port\n";
+
+while (true) {
+    socket_recvfrom($socket, $data, 1024, 0, $client_ip, $client_port);
+    
+    echo "Received data from $client_ip:$data\n";
+
+    if(!checkClient($client_ip)){
+        addClient(new Client($client_ip));
+    }
+
+    $response = processRequest($data, $client_ip);
+
+    socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
+}
+
+socket_close($socket);
+
+function processRequest($request, $client_ip)
 {
+    global $admin_ip;
+    if(substr($request, 0, 1) == "/") {
+        switch($request) {
+            case "/":
+                $response = "Command format is: /{Command}. Type /help for the list of commands!";
+                break;
+            case "/help":
+                $response = "Available commands are:<br>/files<br>/disconnect";
+                break;
+            case "/files":
+                if ($client_ip == $admin_ip) {
+                    $files = scandir(__DIR__."/files");
+                    $response = "Files on server: " . implode(", ", $files);
+                } else {
+                    $response = "You don't have permission for this command!";
+                }
+                break;
+            case "/clients":
+                $response = printClients();
+                break;
+            case "/exec":
+                $response = "Please specify the file: /exec {File Name}!";
+                break;
+            case substr($request, 0, 5) == "/exec":
+                $file = substr($request,6);
+                $fileDirectory = __DIR__."/files"."/".$file;
+                if (file_exists($fileDirectory)) {
+                    $fullPath = realpath($fileDirectory);
+                    exec ("start \"\" \"$fullPath\"", $output,$returnCode);
+                    if($returnCode === 0){
+                        $response = "The file has been successfully executed!";
+                    }else {
+                        $response = "There was an error while executing the file! Please try again.";
+                    }
+                }else{
+                    $response = "The specified file does not exist!";
+                }
+                break;
+            case "/create":
+                $response = "Full command is: /create {file or fold} {File Name}";
+                break;
+            case substr($request, 0, 7) == "/create":
+                $type = substr($request, 8,4);
+                $fileName = substr($request, 13);
+                if($type == "file"){
+                    if($fileName != ""){
+                        $fileDirectory = __DIR__."/files"."/".$fileName.".txt";
+                        if(file_exists($fileDirectory)) {
+                            $response = "The file already exists.Please choose a different name!";
+                        }else{
+                            fopen($fileDirectory, "w") or die("Unable to open file!");
+                            $response = "/ttf".$fileDirectory;
+                        }
+                    }else{
+                        $response = "Please specify the name of the file you want to create!";
+                    }
+                }else if($type == "fold"){
+                    if($fileName != ""){
+                        $folderDirectory = __DIR__."/files"."/".$fileName;
+                        if(file_exists($folderDirectory) && is_dir($folderDirectory)) {
+                            $response = "The folder already exists.Please choose a different name!";
+                        }else{
+                            mkdir($folderDirectory, 0777, true);
+                            $response = "The folder has been successfully created!";
+                        }
+                    }else{
+                        $response = "Please specify the name of the folder you want to create!";
+                    }
+                }
+                break;
+            case substr($request,0,4) == "/ttf":
+                $fileName = substr($request, 4,strpos($request, ',')-4);
+                $message = substr(strstr($request, ","), 1);
+                $myfile = fopen($fileName, "w") or die("Unable to open file!");
+                fwrite($myfile, $message);
+                fclose($myfile);
+                $response = "File has been created!";
+                break;
+            case $request == "/delete":
+                $response = "Full command is: /delete {File Name}";
+                break;
+            case substr($request,0,7) == "/delete":
+                $file = __DIR__."/"."files/".substr($request,8);
+                if(file_exists($file)){
+                    unlink($file);
+                    $response = "The file has been successfully removed!";
+                }else{
+                    $response = "The file you are trying to delete doesn't exist!";
+                }
+                break;
+            case "/disconnect":
+                $response = "Goodbye!";
+                removeClient(new Client($client_ip));
+                break;
+            default:
+                $response = "Invalid command!";
+                break;
+        }
+    }else{
+        $response = "Message received from the server!";
+    }
+    return $response;
+}
+
+function checkClient($c_ip){
     global $clients;
     $ClientInList = false;
-    for ($i = 0; $i < sizeof($clients); $i++) {
-        if ($clients[$i]->ip == $c_ip) {
+    for($i = 0; $i < sizeof($clients); $i++){
+        if ($clients[$i]->ip == $c_ip){
             $ClientInList = true;
         }
     }
@@ -34,79 +155,31 @@ function checkClient($c_ip)
     return $ClientInList;
 }
 
-function addClient(Client $client)
-{
+function addClient(Client $client){
     global $clients;
-    $client->alias = "U" . sizeof($clients) + 1;
-    array_push($clients, $client);
+    $client->alias = "User ".rand(1000000000,9999999999);
+    array_push( $clients, $client );
 }
 
-function printClients()
-{
+function removeClient(Client $client) {
     global $clients;
-    $response = "Clients connected: \n";
-    for ($i = 0; $i < sizeof($clients); $i++) {
-        $response = $response . $clients[$i]->alias . " (" . $clients[$i]->ip . ":" . $clients[$i]->port . ")\n";
-    }
-    return $response;
-}
 
-socket_bind($socket, $server_ip, $server_port);
-
-echo "Server listening on $server_ip:$server_port\n";
-while (true) {
-    socket_recvfrom($socket, $data, 1024, 0, $client_ip, $client_port);
-    if (!checkClient($client_ip)) {
-        addClient(new Client($client_ip, $client_port));
-    }
-    echo "Received data from $client_ip:$client_port: $data\n";
-
-
-    $response = processRequest($data, $client_ip);
-
-    socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
-}
-
-function processRequest($request, $client_ip)
-{
-    global $admin_ip;
-    if (substr($request, 0, 1) == "/") {
-        if ($request == "/") {
-            $response = "Command format is: /{Command}. Type /help for the list of commands!";
-        } else if ($request == "/files") {
-            if ($client_ip == $admin_ip) {
-                $files = scandir(__DIR__);
-                $response = "Files on server: " . implode(", ", $files);
-            } else {
-                $response = "You don't have permission for this command!";
-            }
-        } else if ($request == "/help") {
-            $response = "Available commands are:\n/files\n/disconnect";
-        } else if ($request == "/disconnect") {
-            $response = "Good bye!";
-        } else if ($request == "/clients") {
-            $response = printClients();
-        } else if (substr($request, 0, 5) == "/exec") {
-            $file = substr($request, 6);
-            $fileDirectory = __DIR__ . "/" . $file;
-            if (file_exists($fileDirectory)) {
-                exec($file, $output, $returnCode);
-                if ($returnCode === 0) {
-                    $response = "The file has been successfully executed!";
-                } else {
-                    $reponse = "There was an error while executing the file! Please try again.";
-                }
-            } else {
-                $response = "The specified file does not exist!";
-            }
-        } else {
-            $response = "Invalid command!";
+    foreach ($clients as $key => $existingClient) {
+        if ($existingClient->ip == $client->ip) {
+            unset($clients[$key]);
         }
-    } else {
-        $response = "Message received from the server!";
     }
+    $clients = array_values($clients);
+}
 
+function printClients(){
+    global $clients;
+    $response = "Clients connected: ";
+    for( $i = 0; $i < sizeof($clients); $i++){
+        $response = $response."<br>".$clients[$i]->alias." (".$clients[$i]->ip.")";
+    }
     return $response;
 }
 
-socket_close($socket);
+
+?>
